63936c0bea77e977bbabdad6b6e268d2657d4885
b0pez <blopez4434@gmail.com>
2022-12-02 23:34:15 -0300

    validar campos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,27 @@
 const createUser = (req, res) => {
 
+    const { name, email, password } = req.body;
+
+    res.json({
+        message: 'createUser',
+        name,
+        email,
+        password
+    })
+
 }
 
 const loginUsuario = (req, res) => {
+
+    const { email, password } = req.body;
+
 
 }
 
 const revalidarToken = (req, res) => {
 
+    const { _token } = req.body;
+    
 }
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,24 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
+const { validarCampos } = require('../middlewares/validar-campos');
 const { createUser, loginUsuario, revalidarToken } = require('../controllers/auth');
 
-router.post('/new', createUser);
+router.post('/new',[
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+    validarCampos
+],createUser);
 
-router.post('/', loginUsuario);
+router.post('/', [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+    validarCampos
+], loginUsuario);
 
 router.get('/renew', revalidarToken);
 
